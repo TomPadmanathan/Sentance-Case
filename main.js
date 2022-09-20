3d9cc56021df6c4b6eecb1b612f5fd7b381856ad
Tom Padmanathan <padmanathantom@gmail.com>
2022-09-20 14:49:49 +0100

    Cleaned up code

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 // Get Word And Character Count Function
 function getWordCharacterCount(){
-    let text = document.getElementById('textBox').value
-    let wordCount = text.split(' ').length
-    let characterCount = text.length
-    if(characterCount === 0) {
-        wordCount = 0
-    }
+    let wordCount = document.getElementById('textBox').value.split(' ').length
+    let characterCount = document.getElementById('textBox').value.length
+    if(characterCount === 0) wordCount = 0
     document.getElementById('wordCount').innerHTML='Word count: ' + wordCount
     document.getElementById('characterCount').innerHTML='Character count: ' + characterCount
 }
@@ -23,32 +20,23 @@ function lowerCase(){
 // Alternating Case Function
 function alternatingCase(){
     document.getElementById('textBox').value=document.getElementById('textBox').value.split('').map((c,i) => 
-    i % 2 == 0 ? c.toLowerCase() : c.toUpperCase()
-    ).join('')
+    i % 2 == 0 ? c.toLowerCase() : c.toUpperCase()).join('')
 }
 
 // Capitalized Case Function
 function capitalizedCase(){
     let text = document.getElementById('textBox').value.toLowerCase().split(" ")
-    for(let i = 0; i< text.length; i++){
-        text[i] = text[i][0].toUpperCase() + text[i].slice(1)
-    }
+    for(let i = 0; i< text.length; i++) text[i] = text[i][0].toUpperCase() + text[i].slice(1)
     document.getElementById('textBox').value=text.join(" ")
 }
 
 // Invert Case Function
 function invertCase(){
-    let text = document.getElementById('textBox').value
     let invertCase = ''
-    let index = 0
-    while (index < text.length) {
-        let lowerUpper = text.charAt(index)
-        if (lowerUpper == lowerUpper.toUpperCase()) {
-            lowerUpper = lowerUpper.toLowerCase()
-        } else {
-            lowerUpper = lowerUpper.toUpperCase()
-        }
-        index += 1;
+    for(let i = 0; i < document.getElementById('textBox').value.length; i++) {
+        let lowerUpper = document.getElementById('textBox').value.charAt(i)
+        if (lowerUpper == lowerUpper.toUpperCase()) lowerUpper = lowerUpper.toLowerCase()
+        else lowerUpper = lowerUpper.toUpperCase()
         invertCase += lowerUpper; 
     }
     document.getElementById('textBox').value=invertCase
